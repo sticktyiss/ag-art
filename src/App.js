3b1477ccbded7d7222d8b0bf349ca775443e7055
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-20 16:09:43 -0700

    basic component layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
