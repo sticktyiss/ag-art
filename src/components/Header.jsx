b85aa1e8d528ab62d7d589b042bc64f7f0ae1bed
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-20 17:30:06 -0700

    Working on Header, break 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import {NavLink} from "react-router-dom"
+// TODO: npm i react-router-dom
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  const styleActiveLink = ({ isActive }) => {
+    return {
+      color: isActive ? "" : "", //TODO: Style this out
+    };
+  };
+
+  const scroll = () => window.scrollTo(0,0)
+
+  return (
+    <div>
+      <img src="" />
+      <h1>Alora Goodman</h1>
+      <nav>
+        <NavLink style={styleActiveLink} />
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
