a0a673871bcc07ce4d0bc0353df4f2ed79125d69
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-20 19:23:36 -0700

    Header skeleton done.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 // TODO: npm i react-router-dom
 
 const Header = () => {
-
   const styleActiveLink = ({ isActive }) => {
     return {
       color: isActive ? "" : "", //TODO: Style this out
     };
   };
 
-  const scroll = () => window.scrollTo(0,0)
+  const scroll = () => window.scrollTo(0, 0);
 
   return (
     <div>
       <img src="" />
       <h1>Alora Goodman</h1>
       <nav>
-        <NavLink style={styleActiveLink} />
+        <NavLink style={styleActiveLink}>Home</NavLink>
+        <NavLink style={styleActiveLink}>About</NavLink>
       </nav>
     </div>
   );
