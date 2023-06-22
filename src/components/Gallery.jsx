2134ad261f4cfb0293c932d6c430147e6f620a74
Tyiss Goodman <sticktyiss@gmail.com>
2023-06-22 13:52:55 -0700

    Messed with Header css mainly.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import classes from './css/Gallery.module.css'
+// import ArtPieceCard from './ArtPieceCard' TODO: This will be for when different hosting is figured out besides gh pages.
 
 const Gallery = () => {
   return (
     <div className={classes.main}>
       <h2>Gallery</h2>
+      <ul>
+        <li>
+          <img />
+          <h3>Art Piece</h3>
+          <p>Description ...</p>
+          <h4>Price: $85</h4>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./css/Header.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import logo from "../images/agart-zoomed.jpg";
 
 const Header = () => {
   const navigate = useNavigate();
 
+  const [dropDownMenu, setDropDownMenu] = useState(false);
+
   const styleActiveLink = ({ isActive }) => {
     return {
-      color: isActive ? "#b1be4d" : "#93b341", //TODO: Style this out
+      color: isActive ? "#433070" : "#24153b", //TODO: Style this out
     };
   };
 
@@ -21,23 +24,15 @@ const Header = () => {
       {/* <img src={logo} alt="logo" /> */}
       <h1 onClick={() => navigate("/")}>Alora G Art</h1>
       <nav>
-        <ul>
-          <li>
-            <NavLink style={styleActiveLink} to="/" onClick={scroll}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink style={styleActiveLink} to="gallery" onClick={scroll}>
-              Gallery
-            </NavLink>
-          </li>
-          <li>
-            <NavLink style={styleActiveLink} to="about" onClick={scroll}>
-              About
-            </NavLink>
-          </li>
-        </ul>
+        <NavLink style={styleActiveLink} to="/" onClick={scroll}>
+          Home
+        </NavLink>
+        <NavLink style={styleActiveLink} to="gallery" onClick={scroll}>
+          Gallery
+        </NavLink>
+        <NavLink style={styleActiveLink} to="about" onClick={scroll}>
+          About
+        </NavLink>
       </nav>
     </header>
   );
