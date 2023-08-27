41aef45f3b44142d9626f4e6a281bf1511e4ad7a
Tyiss Goodman <sticktyiss@gmail.com>
2023-08-26 21:01:15 -0700

    Changed the about me and header sections. Transferred cycling images to header.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,42 +2,13 @@ import React, { useState, useEffect } from "react";
 
 import classes from "./css/About.module.css";
 
-// import images for background
-import img1 from "../images/ag-art-flower.jpg";
-import img2 from "../images/lavender-1117275.jpg";
-
 const About = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const images = [img1, img2];
-
-  const changeBackgroundImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(changeBackgroundImage, 10 * 1000); //Change the first # to change how long image is viewed
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  const backgroundImageStyle = {
-    backgroundImage: `url(${images[currentImageIndex]})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width: 'auto',
-    height: '90vh',
-  };
 
   return (
-    <div className={classes.backgroundCycle} style={backgroundImageStyle}>
-      <div>
+      <div className={classes.about}>
         <h2>About Me</h2>
         <p>I'm so cool. Look at me and my stuff.</p>
       </div>
-    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,43 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./css/Header.module.css";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import titleImg from "../images/lavender-1117275.jpg"
 
 import logo from "../images/agart-zoomed.jpg";
+
+import img1 from "../images/ag-art-flower.jpg";
+import img2 from "../images/lavender-1117275.jpg";
+
 
 const Header = () => {
   const navigate = useNavigate();
 
   const scroll = () => window.scrollTo(0, 0);
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [img1, img2];
+
+  const changeBackgroundImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(changeBackgroundImage, 10 * 1000); //Change the first # to change how long image is viewed
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(${images[currentImageIndex]})`,
+  };
+
   return (
     <header className={classes.header}>
       <h1 onClick={() => navigate("/")}>Alora G Art</h1>
-      <div/> {/* Image in the header */}
+      <div style={backgroundImageStyle}/> {/* Image in the header */}
       <nav>
         <NavLink to="/" onClick={scroll}>
           Home
